3816bfb32466e3e195d8bd8a418aa44b3fa0210a
Eli221210 <eliott.bernazeau@gmail.com>
2025-07-01 10:01:46 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "2rem" }}>
+      <h1>Bienvenue sur Draft Foot ⚽</h1>
+      <p>Votre site de draft en 1v1 arrive bientôt !</p>
+    </div>
+  );
+}
+
+export default App;
